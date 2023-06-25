c0116e46037b9b4dbdf830e6f2431c7a07f28bdf
Anton Ždanov <anton@azdanov.dev>
2023-06-25 19:40:12 +0300

    update remap

--- lua/azdanov/remap.lua
+++ lua/azdanov/remap.lua
@@ -8,6 +8,11 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open file explorer" })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line up" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line down" })
 
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Jump down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Jump up and center" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Find next and center" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Find previous and center" })
+
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
