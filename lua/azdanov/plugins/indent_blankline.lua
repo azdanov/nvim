0927f3c4832691d6e65557c9b420505b80352769
Anton Ždanov <anton@azdanov.dev>
2023-06-26 11:10:25 +0300

    change theme and indent symbol

--- lua/azdanov/plugins/indent_blankline.lua
+++ lua/azdanov/plugins/indent_blankline.lua
@@ -1,7 +1,7 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
+    'lukas-reineke/indent-blankline.nvim',
     opts = {
-        char = "┊",
+        char = '╎',
         show_trailing_blankline_indent = false,
     },
 }
