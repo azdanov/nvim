0c366606b81b709e226f488dd63dc133b124cf8e
Anton Ždanov <anton@azdanov.dev>
2023-06-27 10:31:48 +0300

    remove telescope keymaps

--- lua/azdanov/plugins/telescope.lua
+++ lua/azdanov/plugins/telescope.lua
@@ -45,10 +45,8 @@ return {
         end, { desc = "[/] Fuzzily search in current buffer" })
 
         vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
-        vim.keymap.set("n", "<leader>sg", require("telescope.builtin").git_files, { desc = "[S]earch [G]it files" })
         vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
-        vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
-        vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
+        vim.keymap.set("n", "<leader>sw", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
         vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
 
         require("telescope").load_extension("undo")
